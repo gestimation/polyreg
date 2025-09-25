6447f4dd989b36867da6ba187b86697d22e3d355
gestimation <gestimation@gmail.com>
2025-09-25 17:20:59 +0900

    compare proportional model

--- R/functions-old.R
+++ R/functions-old.R
@@ -1,3 +1,298 @@
+
+estimating_equation_proportional_old <- function(
+    formula,
+    data,
+    exposure,
+    ip.weight.matrix,
+    alpha_beta,
+    estimand,
+    optim.method,
+    prob.bound,
+    initial.CIFs = NULL
+) {
+  cl <- match.call()
+  mf <- match.call(expand.dots = TRUE)[1:3]
+  special <- c("strata", "cluster", "offset")
+  Terms <- terms(formula, special, data = data)
+  mf$formula <- Terms
+  mf[[1]] <- as.name("model.frame")
+  mf <- eval(mf, parent.frame())
+  Y <- model.extract(mf, "response")
+  if (!inherits(Y, c("Event", "Surv"))) {
+    t <- rep(0, length(Y))
+    epsilon <- Y
+  } else {
+    t <- Y[, 1]
+    epsilon <- Y[, 2]
+  }
+  if (!is.null(offsetpos <- attributes(Terms)$specials$offset)) {
+    ts <- survival::untangle.specials(Terms, "offset")
+    if (length(ts$vars) > 0) {
+      Terms <- Terms[-ts$terms]
+      offset <- mf[[ts$vars]]
+    } else {
+      offset <- rep(0, nrow(mf))
+    }
+  } else {
+    offset <- rep(0, nrow(mf))
+  }
+  if (is.null(estimand$time.point)) {
+    time.point <- with(data, t[epsilon > 0])
+    time.point <- unique(time.point)
+  } else {
+    time.point <- estimand$time.point
+  }
+
+  y_0_ <- ifelse(epsilon == estimand$code.censoring, 1, 0)
+  y_1_ <- ifelse(epsilon == estimand$code.event1, 1, 0)
+
+  a_ <- as.factor(data[[exposure]])
+  if (estimand$code.exposure.ref==0) {
+    x_a <- as.matrix(model.matrix(~ a_)[, -1])
+  } else {
+    x_a <- as.matrix(rep(1,length(t)) - model.matrix(~ a_)[, -1])
+  }
+  x_l <- model.matrix(Terms, mf)
+  x_la <- cbind(x_l, x_a)
+  i_parameter <- rep(NA, 7)
+  i_parameter <- calculateIndexForParameter(NA,x_l,x_a,length(time.point))
+
+  one <- rep(1, nrow(x_l))
+  a <- as.vector(x_a)
+  score_beta <- NULL
+  score_alpha1 <- 0
+  score_alpha2 <- 0
+  i_time <- 0
+  alpha_beta_i <- rep(NA, i_parameter[7])
+  for (specific.time in time.point) {
+    i_time <- i_time + 1
+    i_para <- i_parameter[1]*(i_time-1)+1
+    alpha_beta_i[1:i_parameter[1]]              <- alpha_beta[i_para:(i_para+i_parameter[1]-1)]
+    alpha_beta_i[i_parameter[2]:i_parameter[3]] <- alpha_beta[i_parameter[8]/2]
+    y_0 <- ifelse(epsilon == estimand$code.censoring | t > specific.time, 1, 0)
+    y_1 <- ifelse(epsilon == estimand$code.event1 & t <= specific.time, 1, 0)
+
+    if (optim.method$computation.order.method=="OLD") {
+      potential.CIFs <- calculatePotentialCIFs_old(alpha_beta,x_a,x_l,offset,epsilon,estimand,optim.method,prob.bound,initial.CIFs)
+    } else {
+      potential.CIFs <- calculatePotentialCIFs_parallel(alpha_beta,x_a,x_l,offset,epsilon,estimand,optim.method,prob.bound,initial.CIFs)
+    }
+    one <- rep(1, nrow(x_l))
+    a <- as.vector(x_a)
+    ey_1 <- potential.CIFs[,2]*a + potential.CIFs[,1]*(one - a)
+    w11 <- 1 / (ey_1 * (1 - ey_1))
+    #    wy_1 <- ip.weight * y_1
+    wy_1 <- ip.weight.matrix[,i_time] * y_1
+    wy_1ey_1 <- w11*(wy_1 - ey_1)
+    d <- cbind(x_l, x_a)
+    residual <- wy_1ey_1
+    #    ret <- as.vector(t(d) %*% residual / nrow(x_l))
+    #    n_col_d <- ncol(d)
+    #    score.matrix <- matrix(NA, nrow=nrow(d), ncol=n_col_d)
+    #    for (j in seq_len(n_col_d)) {
+    #      score.matrix[,j] <- d[,j]*residual
+    #    }
+
+    subscore <- as.vector(t(d) %*% residual / nrow(x_l))
+    tmp1 <- t(subscore[1:i_parameter[1]])
+    score_beta <- cbind(score_beta, tmp1)
+    score_alpha1 <- score_alpha1 + subscore[i_parameter[2]:i_parameter[3]]
+    #    tmp1 <- t(subscore[1:i_parameter[1],])
+    #    tmp2 <- t(subscore[i_parameter[4]:i_parameter[5],])
+    #    score_beta <- cbind(score_beta, tmp1, tmp2)
+    #    score_alpha1 <- score_alpha1 + subscore[i_parameter[2]:i_parameter[3],]
+    #    score_alpha2 <- score_alpha2 + subscore[i_parameter[6]:i_parameter[7],]
+  }
+  #  score <- cbind(score_beta, score_alpha1, score_alpha2)
+  score <- cbind(score_beta, score_alpha1)
+  out <- list(
+    ret   = score,
+    t     = t,
+    y_0   = y_0,
+    y_1   = y_1,
+    y_0_  = y_0_,
+    y_1_  = y_1_,
+    x_a   = x_a,
+    x_l   = x_l,
+    i_parameter = i_parameter
+  )
+  return(out)
+}
+
+
+
+calculatePotentialCIFs_old <- function(
+    alpha_beta_tmp,
+    x_a,
+    x_l,
+    offset,
+    epsilon,
+    estimand,
+    optim.method,
+    prob.bound,
+    initial.CIFs = NULL
+) {
+  i_parameter <- rep(NA_integer_, 7L)
+  i_parameter <- calculateIndexForParameter(i_parameter, x_l, x_a)
+  index1 <- seq_len(i_parameter[1])
+  index23 <- seq.int(i_parameter[2], i_parameter[3])
+  index45 <- seq.int(i_parameter[4], i_parameter[5])
+  index67 <- seq.int(i_parameter[6], i_parameter[7])
+  alpha_1 <- alpha_beta_tmp[index1]
+  beta_tmp_1  <- alpha_beta_tmp[index23]
+  alpha_2 <- alpha_beta_tmp[index45]
+  beta_tmp_2  <- alpha_beta_tmp[index67]
+
+  alpha_tmp_1 <- as.numeric(x_l %*% matrix(alpha_1, ncol = 1) + offset)                                  # ### INDEX-SAFE
+  alpha_tmp_2 <- as.numeric(x_l %*% matrix(alpha_2, ncol = 1) + offset)                                  # ### INDEX-SAFE
+
+  n <- length(epsilon)
+  p0 <- c(
+    sum(epsilon == estimand$code.event1) / n + prob.bound,
+    sum(epsilon == estimand$code.event2) / n + prob.bound,
+    sum(epsilon == estimand$code.event1) / n + prob.bound,
+    sum(epsilon == estimand$code.event2) / n + prob.bound
+  )
+  p0     <- clampP(p0,prob.bound)
+  log_p0 <- log(p0)
+
+  list.CIFs     <- vector("list", nrow(x_l))
+  previous.CIFs <- NULL
+
+  for (i_x in seq_len(nrow(x_l))) {
+    if (i_x > 1) {                                                                                       # ### INDEX-SAFE
+      same_row <- isTRUE(all.equal(
+        as.numeric(x_l[i_x, , drop = FALSE]),                                                            # ### INDEX-SAFE
+        as.numeric(x_l[i_x - 1L, , drop = FALSE])
+      ))
+      if (!is.null(previous.CIFs) && same_row) {
+        list.CIFs[[i_x]] <- previous.CIFs
+        next
+      }
+    }
+
+    if (!is.null(initial.CIFs)) {
+      ip <- as.numeric(initial.CIFs[i_x, , drop = FALSE])
+      if (length(ip) >= 4) ip <- ip[seq_len(4)]                                                          # ### INDEX-SAFE
+      log_p0 <- log(clampP(ip, prob.bound))
+    }
+
+    eq_fn <- function(lp) {
+      estimating_equation_CIFs(
+        log_p        = lp,
+        alpha_tmp_1  = alpha_tmp_1[i_x],
+        beta_tmp_1   = beta_tmp_1,
+        alpha_tmp_2  = alpha_tmp_2[i_x],
+        beta_tmp_2   = beta_tmp_2,
+        estimand     = estimand,
+        optim.method = optim.method,
+        prob.bound   = prob.bound
+      )
+    }
+
+    if (optim.method$inner.optim.method %in% c("optim", "BFGS")) {
+      sol <- optim(par = log_p0, fn = eq_fn, method = "BFGS",
+                   control = list(maxit = optim.method$optim.parameter7,
+                                  reltol = optim.method$optim.parameter6))
+    } else if (optim.method$inner.optim.method == "SANN") {
+      sol <- optim(par = log_p0, fn = eq_fn, method = "SANN",
+                   control = list(maxit = optim.method$optim.parameter7,
+                                  reltol = optim.method$optim.parameter6))
+    } else {
+      stop("Unsupported inner.optim.method")
+    }
+
+    probs <- clampP(exp(sol$par), prob.bound)
+    list.CIFs[[i_x]] <- probs
+    previous.CIFs     <- probs
+  }
+
+  potential.CIFs <- do.call(rbind, list.CIFs)
+  return(potential.CIFs)
+}
+
+
+
+estimating_equation_CIFs <- function(
+    log_p,
+    alpha_tmp_1,
+    beta_tmp_1,
+    alpha_tmp_2,
+    beta_tmp_2,
+    estimand,
+    optim.method,
+    prob.bound
+) {
+  objective_function <- function(log_p) {
+    clog_p <- clampLogP(as.numeric(log_p))
+    if (length(clog_p) < 4L) clog_p <- rep(clog_p, length.out = 4L)
+    exp_lp <- exp(clog_p)
+
+    ## 対の残り確率（log(1 - p1 - p2)）も安全に
+    rem12 <- 1 - exp_lp[1] - exp_lp[2]
+    rem34 <- 1 - exp_lp[3] - exp_lp[4]
+    if (rem12 < prob.bound || rem34 < prob.bound) {
+      lp0102 <- log(prob.bound)
+    } else {
+      lp01   <- log1p(-(exp_lp[1] + exp_lp[2]))
+      lp02   <- log1p(-(exp_lp[3] + exp_lp[4]))
+      lp0102 <- lp01 + lp02
+    }
+
+    ret <- numeric(4L)
+    if (estimand$effect.measure1 == "RR") {
+      ret[1] <- alpha_tmp_1 - clog_p[1] - clog_p[3] + lp0102
+      ret[2] <- beta_tmp_1  - clog_p[3] + clog_p[1]
+    } else if (estimand$effect.measure1 == "OR") {
+      ret[1] <- alpha_tmp_1 - clog_p[1] - clog_p[3] + lp0102
+      ret[2] <- beta_tmp_1  - clog_p[3] + clog_p[1] +
+        log1p(-exp_lp[3]) - log1p(-exp_lp[1])
+    } else if (estimand$effect.measure1 == "SHR") {
+      ret[1] <- alpha_tmp_1 - clog_p[1] - clog_p[3] + lp0102
+      ret[2] <- exp(beta_tmp_1) - ( log1p(-exp_lp[3]) / log1p(-exp_lp[1]) )
+    } else {
+      stop("Invalid effect_measure1. Must be RR, OR or SHR.")
+    }
+
+    if (estimand$effect.measure2 == "RR") {
+      ret[3] <- alpha_tmp_2 - clog_p[2] - clog_p[4] + lp0102
+      ret[4] <- beta_tmp_2  - clog_p[4] + clog_p[2]
+    } else if (estimand$effect.measure2 == "OR") {
+      ret[3] <- alpha_tmp_2 - clog_p[2] - clog_p[4] + lp0102
+      ret[4] <- beta_tmp_2  - clog_p[4] + clog_p[2] +
+        log1p(-exp_lp[4]) - log1p(-exp_lp[2])
+    } else if (estimand$effect.measure2 == '') {
+      ret[3] <- alpha_tmp_2 - clog_p[2] - clog_p[4] + lp0102
+      ret[4] <- exp(beta_tmp_2) - ( log1p(-exp_lp[4]) / log1p(-exp_lp[2]) )
+    } else {
+      stop("Invalid effect_measure2. Must be RR, OR or SHR.")
+    }
+    sum(ret^2)
+  }
+  objective_function(log_p)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 calculatePotentialCIFs_parallel20250921 <- function(
     alpha_beta_tmp,
     x_a,
